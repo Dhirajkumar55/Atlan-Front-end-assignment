5f232181aeef611ef5fe5986735032693dff6f7d
Dhirajkumar Chintada <dhirajkumar@Dhirajkumars-MacBook-Pro.local>
2022-05-05 23:38:55 +0530

    added cors middleware

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const jsonServer = require('json-server')
-
+const cors = require('cors');
 const server = jsonServer.create();
 const Router = jsonServer.router('./json-server/db.json');
 const middlewares = jsonServer.defaults({static:"./build"});
 const port = 5000;
 
 server.use(middlewares);
-server.use(Router)
+server.use(Router);
+server.use(cors());
 
-server.listen(port);
+server.listen(process.env.PORT || 5000);
