754558c995487c660b02b6ae0ef56c1881be3767
Dhirajkumar Chintada <dhirajkumar@Dhirajkumars-MacBook-Pro.local>
2022-05-05 23:26:22 +0530

    fixed the server issue

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const jsonServer = require('json-server')
 const server = jsonServer.create();
 const Router = jsonServer.router('./json-server/db.json');
 const middlewares = jsonServer.defaults({static:"./build"});
-const port = 3000;
+const port = 5000;
 
 server.use(middlewares);
 server.use(Router)
